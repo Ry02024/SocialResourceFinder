22731b3556d83047f37c908aacb18d0184bfa3ed
Ry02024 <162527496+Ry02024@users.noreply.github.com>
2024-04-15 15:52:29 +0900

    Update apiFunctions.js

--- js/apiFunctions.js
+++ js/apiFunctions.js
@@ -21,7 +21,7 @@ export async function embedFn(text, model, api) {
             'Authorization': `Bearer ${api}`
         },
         body: JSON.stringify({ model: model, content: text, taskType: "retrieval_document" })
-    });
+    };
     const data = await response.json();
     return data.embedding;
 }
